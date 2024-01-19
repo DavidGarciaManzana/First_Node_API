59d8ab3dad3342c09bddf42f4049f31326b72b00
David Garcia <davidgarciama@outlook.com>
2024-01-19 11:44:15 -0600

    Custom api error 418 added

--- src/presentation/sentiment-analysis/controller.ts
+++ src/presentation/sentiment-analysis/controller.ts
@@ -14,6 +14,8 @@ export class SentimentAnalysisController{
     analyse = (req:Request, res:Response,library:number) => {
         const {message} = req.body;
         if(!message) {return res.status(400).json({error: `Message property is required`})}
+        if(message.length === 0) {return res.status(418).json({error: `Message or messages inside message property are required`})}
+        console.log(message)
         if(library===1){
             new AnalyseWithNatural(this.sentimentAnalysisRepository)
                 .execute(message)
